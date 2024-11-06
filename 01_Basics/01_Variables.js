12eaa34e85d12be5e4a84e00b46af646b37cd96c
darkdemon5 <yash.sag55@gmail.com>
2024-11-06 16:51:06 +0530

    Learned Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId = 1244543;  //cannot change the value works as constant
+let accountEmail = "Dark@google.com"; // Use let key for varible declaration
+var accountPassword = "4433"; //Almost neve use var keyword because of scope problem
+accountCity = "Mumbai"; // can create varibles without the datatype
+
+
+accountEmail = "ys@google.com";
+accountPassword = "2343"
+accountCity = "Rtn"
+
+/*
+This is block comment
+*/
+
+console.log(accountEmail);
+
+console.table([accountId, accountEmail,accountPassword, accountCity])
+
