c2de05c141bd8facbb0552f39cf610c226d985e3
darkdemon5 <yash.sag55@gmail.com>
2024-11-06 20:19:57 +0530

    Datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // js has been changed over the years. To treat all code as a new version use this (not necessary good to implement)
+
+// alert( 3+4 ) //we use nodejs this is used old js
+
+//Code readability is necessary
+
+//All Data type
+// number => 2 to power of 2
+// bigint same as long in java
+// boolean => true/false
+// null => standalone value
+// undefined => not defined
+// symbol => unique
+
+//object
+
+console.log( typeof undefined); // type of undefined is undefined
+console.log( typeof null); //type of null is object
+
