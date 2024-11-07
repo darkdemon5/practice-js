5043c2d726f136a898df800b809b134c41b14807
darkdemon5 <yash.sag55@gmail.com>
2024-11-08 00:20:34 +0530

    Data Operations and Conversions

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,37 @@
+// ********************** Operations ********************
+
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);  Addition
+// console.log(2-2);  Subtraction
+// console.log(2*2);  Multiplication
+// console.log(2**3); Exponential
+// console.log(2/3);  Division
+// console.log(2%3);  Remainder
+
+let str1 = "Hello";
+let str2 = " Yash";
+
+// console.log(str1+str2);
+
+console.log( 1 + 2);
+console.log( "1" + 2);
+console.log( 1 + "2");
+console.log( "1" + "2");
+console.log( "1" + 5 + 3);
+console.log( 1 + "5" + 3);
+console.log( 1 + 5 + "3");
+console.log( +true); //allowed
+// console.log( true+); //Notallowed
+console.log(+false); //allowed
+console.log(+""); //allowed
+console.log(+"sdf"); //allowed
+
+
+
+
+
---
+++ 01_Basics/05_Comparions.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+
+console.log(null > 0); //Not greater than zero o/p false
+console.log(null == 0); //Is not equal to zero o/p false
+console.log(null >= 0); //But if you see this it is greater than or equal to 0, it is different than above == statement beacuse   o/p true
+// The comparisons(< > =< =>) works different than ==. The comparisons convert the null into a number which is 0.
+console.log(null <= 0); //o/p true
+console.log(null < 0); //o/p false
+
+// === : this is strict check which compares the values and their datatypes eg
+console.log("2" === 2);
+
+
+
+
