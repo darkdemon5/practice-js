57c5a2b28c5237e60615c8ac9390edec0eca50b4
darkdemon5 <yash.sag55@gmail.com>
2024-11-11 20:35:12 +0530

    Math and Num

---
+++ 01_Basics/07_Nums_and_Maths.js
@@ -0,0 +1,36 @@
+const sys = 400;
+
+// console.log(sys);
+
+const ba = new Number(100);
+
+// console.log(ba);
+
+// console.log(ba.toString().length);
+
+const ba1 = 343.524
+
+// console.log(ba1.toFixed(2));
+
+// console.log(ba1.toPrecision(3));
+
+
+const nu = 1000000;
+
+// console.log(nu.toLocaleString('en-EN'));
+
+// console.log(Math.floor(Math.random()*10) +1);
+
+const min = 1;
+const max = 20;
+for (let i = 0; i < 15; i++) {
+    console.log(Math.floor(Math.random()*(max - min + 1) + min));  //Remember this formula   
+}
+
+
+
+
+
+
+
+
