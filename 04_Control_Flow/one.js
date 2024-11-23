fffe57ca11023690421a8abd0ec055e726218f53
darkdemon5 <yash.sag55@gmail.com>
2024-11-23 16:09:37 +0530

    control flow, if-else,switch case

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,57 @@
+
+// const temp = 50
+// if( temp < 50){
+//     console.log("Temp less than 50");
+// }
+// else{
+//     console.log("Temp is more than 50");
+    
+// }
+
+
+// const bal = 1000
+
+// if(bal <500){
+//     console.log("less than 500");
+// }
+// else if (bal < 750) {
+//     console.log("less than 750");
+// }
+// else if (bal < 900) {
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+// const userLoggedIn = true
+// const userHasDebitCard = true
+
+// if (userLoggedIn && userHasDebitCard) {
+//     console.log("Allowed to shop");
+// }
+
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (loggedInFromEmail || loggedInFromGoogle) {
+//     console.log("Allwoed to shop");    
+// }
+
+
+//Nullish Coalescing Operator (??): null undefined 
+
+//it chooses between left or right
+// var = null ?? 10 ans is 10 
+//var = 5 ?? 10 ans is 5
+//var = undefined ?? 10 and is 10 
+//var = null ?? 5 ?? 10 ans is 5
+//var = null ?? undefined ans is undefined
+//var = undefined ?? null ans is null
+
+// let var1;
+// var1 = null ?? undefined
+// console.log(var1);
+
+//Ternary operator
+//condition? Then statement : else statement
---
+++ 04_Control_Flow/truthy_and_falsy_values.js
@@ -0,0 +1,37 @@
+// const email = "h@hitesh.ai"
+
+if (function name() {
+    return false;
+}) {
+    console.log("has email");
+}
+else{
+    console.log("don't have email");
+    
+}
+
+
+//Falsy values :- the values which are considered false in if and else
+
+//false, 0, -0, BigInt 0n(you write 0n in bigInt), "", null, undefined, NaN
+
+
+//Truthy values :- values that are considered true in if and else
+
+// "0"(this is string), 'false'(this is string), " "(This has space so this is true), [](empty array is true), {}(empty object is true), 
+//function(){}(empty function is true)
+
+
+//how to check array is empty
+const arr = []
+
+if (arr.length === 0) {
+    console.log("array is empty");
+}
+
+//how to check object is empty
+const arra = {}
+if(Object.keys(arra).length === 0){
+    console.log("object is empty");
+    
+}
