9433071898cf8e2942f12a92de223e9411839913
darkdemon5 <yash.sag55@gmail.com>
2025-01-05 16:27:17 +0530

    Project One Complete

---
+++ 07_Projects/1-colorChanger/chaiaurcode.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e);
+    console.log(e.target);
+
+    switch (e.target.id) {
+      case "grey":
+        body.style.backgroundColor = "grey";
+        break;
+      case "white":
+        body.style.backgroundColor = "white";
+        break;
+      case "yellow":
+        body.style.backgroundColor = "yellow";
+        break;
+      case "blue":
+        body.style.backgroundColor = "blue";
+        break;
+
+      default:
+        body.style.backgroundColor = "white";
+        break;
+    }
+  });
+});
